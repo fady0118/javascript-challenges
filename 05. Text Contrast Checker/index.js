f02ee95ec130be30ea5b3b385dcb4fc72d40b3c6
Fady Abd Elshahid <121246084+fady0118@users.noreply.github.com>
2025-11-18 15:59:12 +0200

    text contrast checker - update js to check all elements and update the dom

--- 05. Text Contrast Checker/index.js
+++ 05. Text Contrast Checker/index.js
@@ -1,20 +1,31 @@
 // api calling function
-async function checkContrast(fcolor, bcolor) {
-  let url = `https://webaim.org/resources/contrastchecker/?fcolor=${fcolor}&bcolor=${bcolor}&api`;
-  console.log(`checking: ${url}`);
-  try {
-    let response = await fetch(url);
-    let result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-  }
+async function checkContrast(fcolor, bcolor,element) {
+    let url = `https://webaim.org/resources/contrastchecker/?fcolor=${fcolor}&bcolor=${bcolor}&api`;
+    console.log(`checking: ${url}`)
+    try {
+        let response  = await fetch(url);
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        let result = await response.json();
+        console.log(result);
+        element.textContent = `contrast ratio: ${result.ratio} - ${(result.AA)==='pass'?'AA ✅':'AA ❌'}`;
+    } catch (error) {
+        console.log(error)
+    }
 }
-const elements = document.querySelectorAll("main>p");
-const computedStyles = window.getComputedStyle(elements[0]);
-const bcolor = standardize_color(computedStyles.backgroundColor);
-const fcolor = standardize_color(computedStyles.color);
-checkContrast(fcolor, bcolor);
+
+async function checkAll() {
+  console.log("check all fn");
+  const elements = document.querySelectorAll("main>p");
+
+  elements.forEach((element) => {
+    const computedStyles = window.getComputedStyle(element);
+    const bcolor = standardize_color(computedStyles.backgroundColor);
+    const fcolor = standardize_color(computedStyles.color);
+    checkContrast(fcolor, bcolor, element);
+  });
+}
 
 // change format to hex
 function standardize_color(str) {
@@ -23,3 +34,5 @@ function standardize_color(str) {
   let code = ctx.fillStyle.slice(1); // remove '#' -> FFFFFF
   return code.toUpperCase();
 }
+
+document.getElementById('checkAll').addEventListener('click',checkAll)
